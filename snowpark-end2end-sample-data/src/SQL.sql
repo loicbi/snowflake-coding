ded04d6c2585f2e7b22c52dce0131937c187910c
andreloic <loic.andre.bi@gmail.com>
2024-03-31 21:12:39 -0400

    commited 1.0

--- snowpark-end2end-sample-data/src/SQL.sql
+++ snowpark-end2end-sample-data/src/SQL.sql
@@ -10,11 +10,31 @@ create schema if not exists DEMO_DB.ALF_common; -- for file formats sequence obj
 
 
 -- STEP 03 - CREATE INTERNAL STAGE IN SOURCE SCHEMA 
-
 CREATE STAGE IF NOT EXISTS DEMO_DB.ALF_SOURCE.MY_INTERNAL_STAGE;
-
 DESC STAGE DEMO_DB.ALF_SOURCE.MY_INTERNAL_STAGE;
 
 
 -- LIST FILE IN STAGE 
 LS @DEMO_DB.ALF_SOURCE.MY_INTERNAL_STAGE;
+
+
+
+-- STEP 04 - LOAD ORDER FILES TO SATGE LOCATION
+
+-- you can use vs code or command or script python  
+-- following put command can be executed
+/*
+-- csv example
+put file:///tmp/sales/source=IN/format=csv/date=2022-02-22/order-20220222.csv @sales_dwh.source.my_internal_stg/sales/source=IN/format=csv/date=2022-02-22 auto_compress=False overwrite=True, parallel=3 ;
+put file:///tmp/sales/source=IN/format=csv/date=2021-04-26/order-20210426.csv @sales_dwh.source.my_internal_stg/sales/source=IN/format=csv/date=2021-04-26 auto_compress=False overwrite=True, parallel=3 ;
+
+-- json example
+put file:///tmp/sales/source=FR/format=json/date=2022-02-22/order-20220222.json @sales_dwh.source.my_internal_stg/sales/source=FR/format=json/date=2022-02-22 auto_compress=False overwrite=True, parallel=3 ;
+put file:///tmp/sales/source=FR/format=json/date=2021-04-26/order-20210426.json @sales_dwh.source.my_internal_stg/sales/source=FR/format=json/date=2021-04-26 auto_compress=False overwrite=True, parallel=3 ;
+
+-- parquet example
+put file:///tmp/sales/source=US/format=parquet/date=2022-02-22/order-20220222.snappy.parquet @sales_dwh.source.my_internal_stg/sales/source=US/format=parquet/date=2022-02-22 auto_compress=False overwrite=True, parallel=3 ;
+put file:///tmp/sales/source=US/format=parquet/date=2021-04-26/order-20210426.snappy.parquet @sales_dwh.source.my_internal_stg/sales/source=US/format=parquet/date=2021-04-26 auto_compress=False overwrite=True, parallel=3 ;
+*/
+
+
