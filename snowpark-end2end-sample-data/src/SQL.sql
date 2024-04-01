54ddc4771552f5fc4d0f5e35b2ad24c469e39e4a
andreloic <loic.andre.bi@gmail.com>
2024-03-31 20:42:15 -0400

    COMMIT 2024-03-31

--- snowpark-end2end-sample-data/src/SQL.sql
+++ snowpark-end2end-sample-data/src/SQL.sql
@@ -1,3 +1,5 @@
+-- STEP 02 - CREATE SCHEMA 
+
 SELECT CURRENT_WAREHOUSE();
 
 create schema if not exists DEMO_DB.ALF_SOURCE; -- will have source stage etc
@@ -5,3 +7,14 @@ create schema if not exists DEMO_DB.ALF_CURATED; -- data curation and de-duplica
 create schema if not exists DEMO_DB.ALF_consumption; -- fact & dimension
 create schema if not exists DEMO_DB.ALF_audit; -- to capture all audit records
 create schema if not exists DEMO_DB.ALF_common; -- for file formats sequence object etc
+
+
+-- STEP 03 - CREATE INTERNAL STAGE IN SOURCE SCHEMA 
+
+CREATE STAGE IF NOT EXISTS DEMO_DB.ALF_SOURCE.MY_INTERNAL_STAGE;
+
+DESC STAGE DEMO_DB.ALF_SOURCE.MY_INTERNAL_STAGE;
+
+
+-- LIST FILE IN STAGE 
+LS @DEMO_DB.ALF_SOURCE.MY_INTERNAL_STAGE;
